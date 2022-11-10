4788efa5d977e092bb0c74d79939178ba3ab95b5
jaime8793 <109973167+jaime8793@users.noreply.github.com>
2022-11-10 03:21:04 +0300

    Completed Assignment

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,7 @@
 # Require statements should go here.
+# This file is loaded before any other file in the application.
+require_relative '../lib/garden'
+require_relative '../lib/plant'
 
 # Then any files that need to load the files required here can
 # require THIS file, environment.rb, and get access to everything
